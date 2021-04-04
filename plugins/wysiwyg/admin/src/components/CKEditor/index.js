b951a168442c87051bb837c15214a96233853ff8
mohamed <mmmmali654321@gmail.com>
2021-04-04 11:17:26 +0200

    first commit

--- plugins/wysiwyg/admin/src/components/CKEditor/index.js
+++ plugins/wysiwyg/admin/src/components/CKEditor/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import CKEditor  from 'ckeditor4-react-advanced';
-
+//import ClassicEditor from '@akilli/ckeditor4-build-classic'
+//import CKEditor from '@ckeditor/ckeditor5-react';
+import '@ckeditor/ckeditor5-build-classic/build/translations/ar'
+//import ClassicEditor from '@akilli/ckeditor4-build-classic'
 import styled from 'styled-components';
+import ckeditor from 'ckeditor4-react-advanced';
+import ClassicEditor from 'ckeditor5-build-full-plugins'
 
 const Wrapper = styled.div`
   .ck-editor__main {
@@ -17,24 +22,30 @@ const Wrapper = styled.div`
 
 
 
+CKEditor.editorConfig = function( config ) {
+  
+
+	config.toolbar = [
+		{ name: 'document', items: [ 'Source', '-', 'Save', 'NewPage', 'ExportPdf', 'Preview', 'Print', '-', 'Templates' ] },
+		{ name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+		{ name: 'editing', items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
+		{ name: 'forms', items: [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ] },
+		'/',
+		{ name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'CopyFormatting', 'RemoveFormat' ] },
+		{ name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
+		{ name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
+		{ name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
+		'/',
+		{ name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+		{ name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+		{ name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
+		{ name: 'about', items: [ 'About' ] }
+	];
+};
 
 
-const config=[
-  { name: 'document', items: [ 'Source', '-', 'Save', 'NewPage', 'ExportPdf', 'Preview', 'Print', '-', 'Templates' ] },
-  { name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
-  { name: 'editing', items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
-  { name: 'forms', items: [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ] },
-  '/',
-  { name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'CopyFormatting', 'RemoveFormat' ] },
-  { name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
-  { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
-  { name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
-  '/',
-  { name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
-  { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
-  { name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
-  { name: 'about', items: [ 'About' ] }
-];
+
+
 
 
 
@@ -43,11 +54,17 @@ const Editor = ({ onChange, name, value }) => {
   return (
     <Wrapper>
       <CKEditor
-        //  editor={ClassicEditor}
-        //  config={config}
+          // editor={ClassicEditor}
+         config={
+           {
+            language:'ar'
+           }
+             
+      }
         data={value}
-        onChange={(event, editor) => {
-          const data = editor.getData();
+        
+        onChange={(event) => {
+          const data = event.editor.getData();
           onChange({ target: { name, value: data } });
         }}
       />
